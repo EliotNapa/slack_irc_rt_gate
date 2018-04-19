b046e728695489d40f6f49fe81aab5ffb054d4c0
Miyazaki <ryo.miyazaki@giraffe.co.jp>
2018-04-19 11:49:32 +0900

    original irc code added

---
+++ irc_client_py3.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# by bl4de | github.com/bl4de | twitter.com/_bl4de | hackerone.com/bl4de
+"""
+MIT License
+
+Copyright (c) 2017 Rafal Janicki
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import socket
+import threading
+import sys
+
+
+def usage():
+    print("IRC simple Python client | by bl4de | github.com/bl4de | twitter.com/_bl4de | hackerone.com/bl4de\n")
+    print("$ ./irc_client.py USERNAME CHANNEL\n")
+    print("where: USERNAME - your username, CHANNEL - channel you'd like to join (eg. channelname or #channelname)")
+
+
+def channel(channel):
+    if channel.startswith("#") == False:
+        return "#" + channel
+    return channel
+
+# helper function used as thread target
+def print_response():
+    resp = client.get_response()
+    if resp:
+        msg = resp.strip().split(":")
+        print("< {}> {}".format(msg[1].split("!")[0], msg[2].strip()))
+
+
+class IRCSimpleClient:
+
+    def __init__(self, username, channel, server="irc.freenode.net", port=6667):
+        self.username = username
+        self.server = server
+        self.port = port
+        self.channel = channel
+
+    def connect(self):
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn.connect((self.server, self.port))
+
+    def get_response(self):
+        return self.conn.recv(512).decode("utf-8")
+
+    def send_cmd(self, cmd, message):
+        command = "{} {}\r\n".format(cmd, message).encode("utf-8")
+        self.conn.send(command)
+
+    def send_message_to_channel(self, message):
+        command = "PRIVMSG {}".format(self.channel)
+        message = ":" + message
+        self.send_cmd(command, message)
+
+    def join_channel(self):
+        cmd = "JOIN"
+        channel = self.channel
+        self.send_cmd(cmd, channel)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        usage()
+        exit(0)
+    else:
+        username = sys.argv[1]
+        channel = channel(sys.argv[2])
+
+    cmd = ""
+    joined = False
+    client = IRCSimpleClient(username, channel)
+    client.connect()
+
+    while(joined == False):
+        resp = client.get_response()
+        print(resp.strip())
+        if "No Ident response" in resp:
+            client.send_cmd("NICK", username)
+            client.send_cmd(
+                "USER", "{} * * :{}".format(username, username))
+
+        # we're accepted, now let's join the channel!
+        if "376" in resp:
+            client.join_channel()
+
+        # username already in use? try to use username with _
+        if "433" in resp:
+            username = "_" + username
+            client.send_cmd("NICK", username)
+            client.send_cmd(
+                "USER", "{} * * :{}".format(username, username))
+
+        # if PING send PONG with name of the server
+        if "PING" in resp:
+            client.send_cmd("PONG", ":" + resp.split(":")[1])
+
+        # we've joined
+        if "366" in resp:
+            joined = True
+
+    while(cmd != "/quit"):
+        cmd = input("< {}> ".format(username)).strip()
+        if cmd == "/quit":
+            client.send_cmd("QUIT", "Good bye!")
+        client.send_message_to_channel(cmd)
+
+        # socket conn.receive blocks the program until a response is received
+        # to prevent blocking program execution, receive should be threaded
+        response_thread = threading.Thread(target=print_response)
+        response_thread.daemon = True
+        response_thread.start()
