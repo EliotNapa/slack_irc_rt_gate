f1170126417c7906a1c437321747b9324b7a259d
Miyazaki <ryo.miyazaki@giraffe.co.jp>
2018-04-20 13:58:07 +0900

    irc modification added

---
+++ slackbot_settings-template.py
@@ -0,0 +1,12 @@
+API_TOKEN = "<slack api-token>"
+DEFAULT_REPLY = "Sorry but I didn't understand you"
+
+PLUGINS = [
+    'ircrtgate.hello',
+]
+SLACK_CHNNEL = '<target slack channel>'
+
+IRC_SERVER = '<irc server>'
+IRC_PORT = 6667
+IRC_CHANNEL = '<irc channel name>
+IRC_NICKNAME = '<irc bot name>'
