16d62364aa7a5d522ade21a4e4ce8409d2105981
EliotNapa <17310184+EliotNapa@users.noreply.github.com>
2018-05-16 10:09:12 +0900

    short commend added.

--- slackbot_settings-template.py
+++ slackbot_settings-template.py
@@ -1,12 +1,12 @@
-API_TOKEN = "<slack api-token>"
-DEFAULT_REPLY = "Sorry but I didn't understand you"
+API_TOKEN = "<slack api-token>" #set your api token
+DEFAULT_REPLY = "Sorry but I didn't understand you" #no need to edit
 
 PLUGINS = [
-    'ircrtgate.hello',
+    'ircrtgate.hello', #no need to edit
 ]
-SLACK_CHNNEL = '<target slack channel>'
+SLACK_CHNNEL = '<target slack channel>' #without '#' like 'general'
 
-IRC_SERVER = '<irc server>'
+IRC_SERVER = '<irc server>' #like 'irc.ircnet.ne.jp'
 IRC_PORT = 6667
-IRC_CHANNEL = '<irc channel name>'
+IRC_CHANNEL = '<irc channel name>' #with '#' like '#slack-irc'
 IRC_NICKNAME = '<irc bot name>'
