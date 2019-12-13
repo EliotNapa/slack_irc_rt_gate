0fca0893a4fe5ea1e2388d1408fe791ac3a7d72c
EliotNapa <17310184+EliotNapa@users.noreply.github.com>
2019-12-13 14:58:36 +0900

    support for fragment of url.

--- ircrtgate/hello.py
+++ ircrtgate/hello.py
@@ -21,6 +21,7 @@ def hello_send(message):
 
 def url_convert(src_string):
     """
+    encode last path of url
     """
 
     result = src_string
@@ -36,21 +37,26 @@ def url_convert(src_string):
 
     if match_enc:
         if Encode_Path_only:
-            last_pos = result.rfind('/')
-            param_pos = result.rfind('?')
-            if 0 < last_pos:
-                if 0 < param_pos:
-                    result = '<{0}/{1}?{2}>'.format(
-                            result[1:last_pos],
-                            urllib.parse.quote(result[last_pos+1:param_pos]),
-                            result[param_pos+1:-1]
-                        )
-                else:
-                    result = '<{0}/{1}>'.format(
-                            result[1:last_pos],
-                            urllib.parse.quote(result[last_pos+1:-1])
-                        )
-        
+            fragment_pos = result.rfind('#')
+
+            work_src = result
+            fragment_part = ''
+            if 0 < fragment_pos:
+                fragment_part = work_src[fragment_pos:-1]
+                work_src = work_src[:fragment_pos]
+
+            param_part = ''
+            param_pos = work_src.rfind('?')
+            if 0 < param_pos:
+                param_part = work_src[param_pos:-1]
+                work_src = work_src[:param_pos]
+
+            path_part = ''
+            path_pos =  work_src.rfind('/')
+            if 0 < path_pos:
+                path_part = urllib.parse.quote(work_src[path_pos:-1])
+                work_src = work_src[:path_pos]
+            result = work_src + path_part + param_part + fragment_part + '>'
         else:
             find_pos = result.find('://')
             if 0 < find_pos:
